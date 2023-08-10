e4dde6e0e916f30a1cafcb89199b7f71d0903dd2
Julia Nemkina <nemkina.julia@mail.ru>
2023-08-10 19:15:34 +0300

    new 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+import ru.netology.services.RestService;
+
+public class Main {
+    public static void main(String[] args) {
+        RestService service = new RestService();
+        int months = service.calculate(100_000, 60_000, 150_000);
+        System.out.println(months);
+    }
 }
